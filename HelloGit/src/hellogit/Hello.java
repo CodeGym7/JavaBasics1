f516caa82161e6f66bfbc3f2617f2c55dd3bb604
CodeGym7 <swathibh16@gmail.com>
2018-11-30 13:16:56 +0100

    Added branch comment

--- HelloGit/src/hellogit/Hello.java
+++ HelloGit/src/hellogit/Hello.java
@@ -1,5 +1,6 @@
 package hellogit;
 //first git program
+//Working on first branch not on master
 public class Hello {
 	public static void main(String[] args){
 		System.out.println("This is first git java program!!yay!!");
