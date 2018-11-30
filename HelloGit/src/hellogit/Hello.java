4b66c1a23a8de5af4ea044c42d51c6c6f689ddd4
CodeGym7 <44864742+CodeGym7@users.noreply.github.com>
2018-11-30 13:42:16 +0100

    Update Hello.java

--- HelloGit/src/hellogit/Hello.java
+++ HelloGit/src/hellogit/Hello.java
@@ -2,7 +2,8 @@ package hellogit;
 //first git program
 //Working on first branch not on master
 //Added a comment directly on github.
-//Added a comment once again
+
+//deleted one comment
 public class Hello {
 	public static void main(String[] args){
 		System.out.println("This is first git java program!!yay!!");
