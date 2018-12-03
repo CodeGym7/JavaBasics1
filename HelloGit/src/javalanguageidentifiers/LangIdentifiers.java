0883a1eac8ccb7dbbde3e306825dda6cc9ee98d6
CodeGym7 <swathibh16@gmail.com>
2018-12-03 13:08:21 +0100

    Created a new package javalanguageidentifiers ,added one program

---
+++ HelloGit/src/javalanguageidentifiers/LangIdentifiers.java
@@ -0,0 +1,11 @@
+package javalanguageidentifiers;
+
+public class LangIdentifiers {
+	public static void main(String args[]){
+		int String=333;
+		System.out.println(String);
+		int xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx=99;
+		System.out.println(xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx);
+	}
+
+}
