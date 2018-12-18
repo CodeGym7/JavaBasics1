e3527d2e907e8bddfeec8c4d14d4253fbad95c0c
CodeGym7 <swathibh16@gmail.com>
2018-12-18 12:35:36 +0100

    Modified changes in LanguageIdentifiers class

--- HelloGit/src/javalanguageidentifiers/LangIdentifiers.java
+++ HelloGit/src/javalanguageidentifiers/LangIdentifiers.java
@@ -2,10 +2,36 @@ package javalanguageidentifiers;
 
 public class LangIdentifiers {
 	public static void main(String args[]){
-		int String=333;
-		System.out.println(String);
-		int xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx=99;
-		System.out.println(xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx);
+//		int String=333;
+//		System.out.println(String);
+//		int xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx=99;
+//		System.out.println(xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx);
+//	    double d=123.456d;
+//	   char ch=0xFace;
+//	   System.out.println(ch);
+//	   char ch1=0777;
+//	   System.out.println(ch1);
+//	   char ch2=78;
+//	   System.out.println(ch2);
+//	   int x='a';
+//	   int[] y=new int[2];
+//	   System.out.println(y.getClass().getName());
+//	    
+//	   int[][] z=new int[2][3];
+//	   System.out.println(z.getClass().getName());
+//	   
+//	   char[] s=new char[2];
+//	   System.out.println(s.getClass().getName());
+	   
+	   //int[] d=new int[-2];
+		//int[] f=new int[2147483647];
+	   
+	   
+	   
+	   
+	   
+	    
+		
 	}
 
 }
